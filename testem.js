cc8b843a13d8fdf2e163735cefed0886aead10d3
Randall Morey <randall@randallmorey.com>
2017-08-31 15:23:35 -0400

    test(parallelization): parallelize tests and run in phantomjs, chrome, and firefox

--- testem.js
+++ testem.js
@@ -2,11 +2,23 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
+  parallel: -1,
   launch_in_ci: [
-    'PhantomJS'
+    'PhantomJS',
+    'Chrome',
+    'Firefox'
   ],
   launch_in_dev: [
     'PhantomJS',
-    'Chrome'
-  ]
+    'Chrome',
+    'Firefox'
+  ],
+  'browser_args': {
+    'Chrome': [
+      '--headless',
+      '--disable-gpu',
+      '--remote-debugging-port=9222',
+      '--window-size=1440,900'
+    ]
+  }
 };
