547d7a97d00de2feb0525657db965efed43f6659
Randall Morey <randallmorey@randallmorey.com>
2018-01-16 10:38:44 -0500

    test(browsers): remove phantomjs support; add firefox headless

--- testem.js
+++ testem.js
@@ -4,20 +4,21 @@ module.exports = {
   disable_watching: true,
   parallel: -1,
   launch_in_ci: [
-    'PhantomJS',
     'Chrome',
     'Firefox'
   ],
   launch_in_dev: [
-    'PhantomJS',
     'Chrome',
     'Firefox'
   ],
-  'browser_args': {
-    'Chrome': [
+  browser_args: {
+    Chrome: [
       '--headless',
-      '--disable-gpu',
       '--remote-debugging-port=9222',
+      '--window-size=1440,900'
+    ],
+    Firefox: [
+      '-headless',
       '--window-size=1440,900'
     ]
   }
